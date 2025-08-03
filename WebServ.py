249c93ce712437592eb13d666bc96af5a40f3795
magr1k5 <m.uriadnickov2017@yandex.ru>
2025-08-03 09:16:34 +0300

    Initial commit

---
+++ WebServ.py
@@ -0,0 +1,15 @@
+from flask import Flask, request
+
+recr = Flask(__name__)
+
+@recr.route('/')
+def hello():
+    name = request.args.get('name', 'друг')  # если параметр не передан, используем 'друг'
+    message = request.args.get('message', '')
+
+    # Создаём ответ
+    response = f"Hello {name}! {message}"
+    return response
+
+if __name__ == '__main__':
+    recr.run(host='0.0.0.0', port=5000)
